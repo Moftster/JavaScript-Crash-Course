fe0ee18e1ba16f4b184c80d76989bc9462027e84
David Moftakhar <david@sellmysnaps.com>
2019-08-30 08:08:13 +0100

    Using methods

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 function Person(firstName, lastName, dob) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.dob = dob;
+  this.dob = new Date(dob);
+  this.getBirthYear = function() {
+    return this.dob.getFullYear();
+  }
+  this.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 // Instantiate object
@@ -10,4 +16,5 @@ const person1 = new Person('John', 'Doe', '8-5-1983');
 const person2 = new Person('Dave', 'Jones', '8-7-1993');
 const person3 = new Person('Mary', 'Phillips', '1-9-1976');
 
-console.log(person2.lastName);
+console.log(person1.getBirthYear());
+console.log(person2.getFullName());
