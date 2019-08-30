4dab94e1ce31c935640a248b522deddaab6c34b4
David Moftakhar <david@sellmysnaps.com>
2019-08-30 08:00:53 +0100

    Constructor function and instantiate the object

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const x = 10;
+// Constructor function
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = dob;
+}
 
-const color = x > 10 ? 'red' : 'blue';
+// Instantiate object
+const person1 = new Person('John', 'Doe', '8-5-1983');
+const person2 = new Person('Dave', 'Jones', '8-7-1993');
+const person3 = new Person('Mary', 'Phillips', '1-9-1976');
 
-switch(color){
-  case 'red':
-    console.log('color is red');
-    break;
-  case 'blue':
-    console.log('color is blue');
-    break;
-  default:
-    console.log("color is not red or blue");
-    break;
-  }
+console.log(person2.lastName);
