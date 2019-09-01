e57e3d2355d2844f8c5e176297f772d55f7451a1
David Moftakhar <david@sellmysnaps.com>
2019-09-01 08:54:23 +0100

    Single and multiple element selectors

---
+++ main.js
@@ -0,0 +1,8 @@
+// Single element selectors
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+// Multiple element selectors
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByClassName('item'));
+console.log(document.getElementsByTagName('li'));
