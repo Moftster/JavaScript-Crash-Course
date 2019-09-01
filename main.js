5fce7e9b01771083a8108758dfe87d91f148548d
David Moftakhar <david@sellmysnaps.com>
2019-09-01 09:34:38 +0100

    Examples of manipulating the dom

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Single element selectors
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('h1'));
+const btn = document.querySelector('.btn');
 
-// Multiple element selectors
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('item'));
-console.log(document.getElementsByTagName('li'));
+btn.addEventListener('mouseout', (e) => {
+  e.preventDefault();
+  document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
