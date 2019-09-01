a9cb8e4f69c96b283a3d15dea226e4698d920760
David Moftakhar <david@sellmysnaps.com>
2019-09-01 08:12:47 +0100

    Using classes

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-// Constructor function
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob);
-  this.getBirthYear = function() {
+// Class
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
+  getBirthYear() {
     return this.dob.getFullYear();
   }
-  this.getFullName = function() {
+  getFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
 }
+
 
 // Instantiate object
 const person1 = new Person('John', 'Doe', '8-5-1983');
 const person2 = new Person('Dave', 'Jones', '8-7-1993');
 const person3 = new Person('Mary', 'Phillips', '1-9-1976');
 
-console.log(person1.getBirthYear());
-console.log(person2.getFullName());
+console.log(person3.getFullName());
+console.log(person3);
