094be1805f0f612337d62b3f23cc60fc3965aa81
David Moftakhar <david@sellmysnaps.com>
2019-09-02 09:24:55 +0100

    Get elements by class name

--- DOM-1/dom.js
+++ DOM-1/dom.js
@@ -24,3 +24,17 @@
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h3>Hi There!</h3>';
 // header.style.borderBottom = 'solid 3px #000';
+
+// GET ELEMENTs BY CLASS NAME
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// items.style.backgroundColor = 'grey'; gives error
+
+for(var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = '#f4f4f4';
+}
