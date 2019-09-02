c2c67fa2275034b49f6c03e51a042d357682282b
David Moftakhar <david@sellmysnaps.com>
2019-09-02 09:15:34 +0100

    Get element by ID functions

--- DOM-1/dom.js
+++ DOM-1/dom.js
@@ -2,14 +2,25 @@
 // console.dir(document);
 // console.log(document.domain);
 // console.log(document.URL);
-// // document.title = "testing new title";
+// document.title = "testing new title";
 // console.log(document.title);
 // console.log(document.doctype);
 // console.log(document.head);
 // console.log(document.body);
 // console.log(document.all);
 // console.log(document.all[10]);
-// // document.all[10].textContent = 'Hello';
+// document.all[10].textContent = 'Hello';
 // console.log(document.forms[0]);
 // console.log(document.links);
 // console.log(document.images);
+
+// GET ELEMENT BY ID
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hi There!</h3>';
+// header.style.borderBottom = 'solid 3px #000';
